c77c936170276102e113f4760eb18c56af6bbb5e
Subham Kumar Maharana <100522730+Subham3099@users.noreply.github.com>
2024-02-24 23:06:25 +0530

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Organization, Item, Pricing
 
 class CalculationForm(forms.Form):
     zone = forms.CharField(max_length=10)
@@ -6,3 +7,35 @@ class CalculationForm(forms.Form):
     total_distance = forms.FloatField()
     item_type = forms.CharField(max_length=10)
     item_id = forms.CharField(max_length=10)
+
+class OrganizationForm(forms.ModelForm):
+    class Meta:
+        model = Organization
+        fields = ['id', 'name']
+        labels = {
+            'id': 'ID',
+            'name': 'Name'
+        }
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['id', 'type', 'description']
+        labels = {
+            'id': 'ID',
+            'type': 'Type',
+            'description': 'Description'
+        }
+
+class PricingForm(forms.ModelForm):
+    class Meta:
+        model = Pricing
+        fields = ['organization', 'item', 'zone', 'base_distance_in_km', 'km_price', 'fix_price']
+        labels = {
+            'organization': 'Organization',
+            'item': 'Item',
+            'zone': 'Zone',
+            'base_distance_in_km': 'Base Distance (km)',
+            'km_price': 'Price per km',
+            'fix_price': 'Fixed Price'
+        }
